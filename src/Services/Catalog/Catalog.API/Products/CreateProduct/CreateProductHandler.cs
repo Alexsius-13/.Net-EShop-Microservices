14c07059f8a275f9240e92cdae877f7f8d06b937
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-12-31 16:27:30 +0400

    Implement CreateProduct Vertical/Slice in Vertical-Slice Architecture using Mapster, MediatR, Carter for Minimal Api Endpoint dynamics. According to CQRS Design Pattern

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,28 @@
-﻿using MediatR;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name, List<string> Categories, 
-	string Description, string ImageUrl, decimal Price) : IRequest<CreateProductResult>;
+	string Description, string ImageUrl, decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
-internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-	public Task<CreateProductResult> Handle(CreateProductCommand request, 
+	public async Task<CreateProductResult> Handle(CreateProductCommand command, 
 		CancellationToken cancellationToken)
 	{
-		throw new NotImplementedException();
+		//create product entity via command object
+		var product = new Product
+		{
+			Name = command.Name,
+			Categories = command.Categories,
+			Description = command.Description,
+			ImageUrl = command.ImageUrl,
+			Price = command.Price
+		};
+
+		//save to db
+
+
+		//return result
+		return new CreateProductResult(Guid.NewGuid());
 	}
 }
