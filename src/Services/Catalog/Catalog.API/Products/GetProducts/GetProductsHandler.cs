b00d9091f3033f05afe1f22359d2e66c84ed4ad4
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-01-02 16:13:35 +0400

    Implemented GetProducts Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+
+internal class GetProductsHandler(IDocumentSession session, 
+	ILogger<GetProductsResult> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+	public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Get Product Query Retrieved", query);
+
+		var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+		return new GetProductsResult(products);
+	}
+}
