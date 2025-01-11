3bd833c68172c7303f810050291ba191515aa044
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-01-11 16:24:52 +0400

    Implement Update Product Vertical/Slice in Catalog Microservice via MediatR, Carter, Mapster

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id, string Name, List<string> Categories,
+	string Description, string ImageUrl, decimal Price) : ICommand<UpdateProductResult>;
+
+public record UpdateProductResult(bool IsSuccess);
+internal class UpdateProductHandler(IDocumentSession session) 
+	: ICommandHandler<UpdateProductCommand, UpdateProductResult>
+{
+	public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+	{
+		var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+
+		if(product is null)
+		{
+			throw new ProductNotFoundException();
+		}
+
+		product.Categories = command.Categories;
+		product.Name = command.Name;
+		product.Description = command.Description;
+		product.ImageUrl = command.ImageUrl;
+		product.Price = command.Price;
+
+		session.Update(product);
+		await session.SaveChangesAsync(cancellationToken);
+
+		return new UpdateProductResult(true);
+	}
+}
