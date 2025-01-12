72bd5e39cd001c87f10f8f78960cd61e76526b07
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-01-12 18:20:03 +0400

    Implement Fluent Validation for Product Creation

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,11 +4,31 @@ public record CreateProductCommand(string Name, List<string> Categories,
 	string Description, string ImageUrl, decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
-internal class CreateProductHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+		RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+		RuleFor(x => x.Categories).NotEmpty().WithMessage("Category is required");
+		RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Image is required");
+		RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greate than 0");
+    }
+}
+internal class CreateProductHandler(IDocumentSession session, IValidator<CreateProductCommand> validator)
+	: ICommandHandler<CreateProductCommand, CreateProductResult>
 {
 	public async Task<CreateProductResult> Handle(CreateProductCommand command, 
 		CancellationToken cancellationToken)
 	{
+		//Validate product fields
+		var result = await validator.ValidateAsync(command, cancellationToken);
+		var errors = result.Errors.Select(r => r.ErrorMessage).ToList();
+		if (errors.Any())
+		{
+			throw new ValidationException(errors.FirstOrDefault());
+		}
+
 		//create product entity via command object
 		var product = new Product
 		{
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 builder.Services.AddCarter();
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMediatR(config =>
 {
 	config.RegisterServicesFromAssembly(typeof(Program).Assembly);
@@ -10,6 +11,7 @@ builder.Services.AddMarten(opt =>
 {
 	opt.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
 
 var app = builder.Build();
 
