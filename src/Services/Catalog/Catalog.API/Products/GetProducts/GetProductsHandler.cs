a4769a89e718d8a7ea4753ecf00a1847c3d47490
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-01-17 19:12:04 +0400

    Implement Pagination For GetAllProducts Endpoint via Marten's Amazing tool ToPagedListAsync

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,7 +1,9 @@
 ﻿
+using Marten.Pagination;
+
 namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsHandler(IDocumentSession session, 
@@ -11,7 +13,8 @@ internal class GetProductsHandler(IDocumentSession session,
 	{
 		logger.LogInformation("Get Product Query Retrieved", query);
 
-		var products = await session.Query<Product>().ToListAsync(cancellationToken);
+		var products = await session.Query<Product>()
+			.ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
 		return new GetProductsResult(products);
 	}
