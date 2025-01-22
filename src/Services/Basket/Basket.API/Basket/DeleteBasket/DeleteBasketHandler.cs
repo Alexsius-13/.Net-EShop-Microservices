b37d045a54698b13daef9817dee3e1c181b74da9
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-01-22 16:37:39 +0400

    Implement Basket Microservice CRUD Operations without storing in database just a skeleton version.

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,6 +1,19 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
-	public class DeleteBasketHandler
+    public DeleteBasketCommandValidator()
+    {
+		RuleFor(x => x.UserName).NotEmpty().WithMessage("Username can not be empty");
+    }
+}
+public class DeleteBasketHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+	public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
 	{
+		return new DeleteBasketResult(true);
 	}
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+	config.RegisterServicesFromAssembly(assembly);
+	config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure the Http request pipeline.
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
