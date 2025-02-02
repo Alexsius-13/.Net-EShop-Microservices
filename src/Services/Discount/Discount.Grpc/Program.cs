bf5e258f72dc7f96d9e930454efb5ca39ab89dca
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-02-02 15:34:52 +0400

    Generate proto buff file in gRPC Service to implement synchronous communication between microservices.

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if(app.Environment.IsDevelopment())
+	app.MapGrpcReflectionService();
+
+
+
+app.Run();
