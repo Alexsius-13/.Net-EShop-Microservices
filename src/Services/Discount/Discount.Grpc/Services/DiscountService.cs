3d5d61adff641a6828eabbe5cd8186da6ac9c506
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-02-07 15:50:10 +0400

    Finish Crud operation in Discount Grpc Service.

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -20,18 +20,42 @@ public class DiscountService(DiscountContext dbContext)
 
 		return couponModel;
 	}
-	public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+	public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
 	{
-		return base.CreateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if (coupon is null)
+			throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid object"));
+
+		dbContext.Coupons.Add(coupon);
+		await dbContext.SaveChangesAsync();
+
+		var couponModel = coupon.Adapt<CouponModel>();
+		return couponModel;
 	}
 
-	public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+	public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
 	{
-		return base.UpdateDiscount(request, context);
+		var coupon = request.Coupon.Adapt<Coupon>();
+		if(coupon is null)
+			throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid object"));
+
+		dbContext.Coupons.Update(coupon);
+		await dbContext.SaveChangesAsync();
+
+		var couponModel = coupon.Adapt<CouponModel>();
+		return couponModel;
 	}
 
-	public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+	public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
 	{
-		return base.DeleteDiscount(request, context);
+		var coupon = await dbContext.Coupons
+						.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+		if(coupon is null)
+			throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid object"));
+
+		dbContext.Coupons.Remove(coupon);
+		await dbContext.SaveChangesAsync();
+
+		return new DeleteDiscountResponse { Success = true };
 	}
 }
