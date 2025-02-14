ed562783996b1483c5bb8ce959e67fc8f3093eaa
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-02-14 19:47:08 +0400

    Implement Get Discount service in Basket microservise via Grpc communication

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.Grpc;
+
+namespace Basket.API.Basket.StoreBasket;
 
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -12,13 +14,25 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 		RuleFor(x => x.Cart.Username).NotEmpty().WithMessage("Username can not be empty");
     }
 }
-public class StoreBasketHandler(IBasketRepository repository)	
+public class StoreBasketHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
 	: ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
 	public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
 	{
+		//TODO: Get discount if exists from Discount.Grpc
+		await DeductDiscount(command.Cart, cancellationToken);
+
 		await repository.StoreBasket(command.Cart, cancellationToken);
 
 		return new StoreBasketResult(command.Cart.Username);
+	}
+
+	private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+	{
+		foreach (var item in cart.Items)
+		{
+			var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName });
+			item.Price -= coupon.Amount;
+		}
 	}
 }
