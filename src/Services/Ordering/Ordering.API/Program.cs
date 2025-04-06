e60d08479b284994ae15842e28acc156fe54faf0
Aleko Kotashvili <alexkotashvili@gmail.com>
2025-04-06 18:27:48 +0400

    Implement Ordering.API Layer Expose Minimal Apis as best practise of modern .Net Application via Carter. Conduct CustomExceptionHandler for better error responses globally. Add health checks to sql server

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -9,13 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
 	.AddApplicationServices()
 	.AddInfrastructureServices(builder.Configuration)
-	.AddApiServices();
+	.AddApiServices(builder.Configuration);
 
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 app.UseApiServices();
+
 if (app.Environment.IsDevelopment())
 {
 	await app.InitializeDataAsync();
